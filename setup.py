aab191443dc10138e16ca15ca20a1feac8ce70ab
Priyadarshi Rishiraj <p.rishiraj@gmail.com>
2024-09-27 12:06:10 +0530

    removed email, added encoding

--- setup.py
+++ setup.py
@@ -8,11 +8,10 @@ setup(
         'termcolor',
     ],
     description='Utility for colored  print and input statements',
-    long_description=open('README.md').read(),
+    long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/roamingsaint/pyColorPrint.git',
     author='Kanad Rishiraj',
-    author_email='p.rishiraj@gmail.com',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
